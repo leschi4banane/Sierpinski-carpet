0c4105bb65001abda2b375454a362bea15a11fe8
leschi4banane <leschi.4.banane@gmail.com>
2023-01-12 20:17:44 +0100

    added esential fetures

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import pygame as pg
 
-def devide(top_left, botton_right, depth=4):
+def devide(top_left, size, depth):
     if depth != 0:
-        segment = (botton_right[0]-top_left[0])/3
-        pg.draw.rect(screen, (0,0,0), pg.Rect(top_left, botton_right), 10)
-
-        #devide(top_left, (top_left[0]+segment, top_left[1]+segment), depth-1)
-        devide((top_left[0]+segment,top_left[1]), (botton_right[0]-segment, top_left[1]+segment), depth-1)
-        #devide((top_left[0]+segment*2,top_left[1]), (botton_right[0], top_left[1]+segment), depth-1)
-        #devide((top_left[0],top_left[1]+segment), (top_left[0]+segment, segment), depth-1)
-        #devide((top_left[0]+segment*2,top_left[1]+segment), (botton_right[0], segment), depth-1)
-        #devide((top_left[0]+segment*2,top_left[0]+segment*2), botton_right, depth-1)
-        #devide((top_left[0],botton_right[1]-segment), (top_left[0]+segment, botton_right[1]), depth-1)
-
-
+        segment = size/3
+        pg.draw.rect(screen, (0,0,0), pg.Rect(top_left[0]+segment, top_left[1]+segment, segment, segment))
+        
+        for x in range(3):
+            for y in range(3):
+                if x==1 and y==1:
+                    pass
+                else:
+                    devide((top_left[0] + segment*x, top_left[1] + segment*y), segment, depth-1)
 
 pg.init()
 
@@ -21,7 +18,7 @@ screen = pg.display.set_mode((800,800))
 screen.fill((255,255,255))
 
 start_1 = (0,0)
-start_2 = 800,800
+start_2 = (800,800)
 
 
 while 1:
@@ -30,6 +27,6 @@ while 1:
         if event.type == pg.QUIT:
             pg.quit()
 
-    devide(start_1, start_2)
+    devide(start_1, 800, 6)
 
     pg.display.flip()
